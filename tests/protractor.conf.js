0ffb0e199e3e319633d96cf4e3fb561595bd1c20
Aurora Velez <avelez@nearsoft.com>
2016-03-28 08:30:09 -0700

    Changed protractor reports path

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -30,8 +30,8 @@ exports.config = {
       onPrepare: function() {
           jasmine.getEnv().addReporter(
               new Jasmine2HtmlReporter({
-                  savePath: './e2e/results',
-                  screenshotsFolder: 'images',
+                  savePath: './tests/e2e/results',
+                  screenshotsFolder: './tests/e2e/results/images',
                   takeScreenshots: true
 
               })
